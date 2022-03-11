ab2f68319d07b34dcfa42b876f6ce5e0bd25ec1e
junehan <junehan.dev@gmail.com>
2022-03-11 18:47:56 +0900

    SOLVED/561-array_partition

--- LC/561-Array_partition-i/arr_partition.py
+++ LC/561-Array_partition-i/arr_partition.py
@@ -1,27 +1,18 @@
 from typing import List, Tuple
-from doctest import testmod
+from functools import reduce
 
 class Solution:
 	def	arrayPairSum(self, nums: List[int]) -> int:
-		"""summation on last iterable in argument, nums.
-		>>> s = Solution();
-		>>> s.arrayPairSum([1,2,3,4])
-		7
-		"""
-		paired_arr = pairArray(nums);
-		return sum(paired_arr[-1]);
+		sorted_nums = sorted(nums);
+		odd_idx_nums = sorted_nums[::2];
+		return sum(odd_idx_nums);
 
-def pairArray(nums: List[int]) -> Tuple[Tuple[int]]:
-	"""pairs an array, produce a zipped tuple with even's with odd's in nums.
-	>>> pairArray([1,2,3,4]);
-	((1, 2), (3, 4))
-	"""
-	return tuple(zip(nums[::2], nums[1::2]));
+def sortArray(nums: List[int]) -> List[int]:
+	return (sorted(nums));
+
+def sliceEven(nums: List[int]) -> List[int]:
+	return (nums[::2]);
 
 if __name__ == "__main__":
-	testmod();
-"""
-	partition = Solution();
-	ret = partition.pairArray([1,2,3,4,5,6,7,8])
-	print(ret);
-"""
+	a_sol = Solution()
+	print(a_sol.arrayPairSum([1,2,3,4]));
