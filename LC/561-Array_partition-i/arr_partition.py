869ab8f5eba88a65efbc11ddc9146fa9a6f2f6f2
junehan <junehan.dev@gmail.com>
2022-03-11 18:49:25 +0900

    CLEAR/561-array_partition/replace function
    REPLACED
       - 2 functions:sort and partition -> 2 Expression

--- LC/561-Array_partition-i/arr_partition.py
+++ LC/561-Array_partition-i/arr_partition.py
@@ -1,17 +1,9 @@
-from typing import List, Tuple
-from functools import reduce
+from typing import List
 
 class Solution:
 	def	arrayPairSum(self, nums: List[int]) -> int:
-		sorted_nums = sorted(nums);
-		odd_idx_nums = sorted_nums[::2];
-		return sum(odd_idx_nums);
-
-def sortArray(nums: List[int]) -> List[int]:
-	return (sorted(nums));
-
-def sliceEven(nums: List[int]) -> List[int]:
-	return (nums[::2]);
+		nums.sort();
+		return sum(nums[::2]);
 
 if __name__ == "__main__":
 	a_sol = Solution()
