e222e559983bb0befbe35dbb8000352740eb904c
junehan <junehan.dev@gmail.com>
2022-03-12 02:12:23 +0900

    ONGOING/49/group_anagram.c

--- LC/49-Group_anagrams/groupAnagrams.c
+++ LC/49-Group_anagrams/groupAnagrams.c
@@ -3,13 +3,34 @@
  * The sizes of the arrays are returned as *returnColumnSizes array.
  * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
  */
-char	***groupAnagrams(char **strs, int strsSize, int* returnSize, int** returnColumnSizes){
-	return (0);
+
+#include <unistd.h>
+#include <stdlib.h>
+
+char	***groupAnagrams(char **strs, int strsSize,
+						int *returnSize, int **returnColumnSizes)
+{
+	char ***ret;
+
+	ret = malloc(sizeof(char ***));
+	
+	return (ret);
 }
+// {{"asc", "csa"}, {"abd"}} -> returnSize = 2
+// {{"asc", "csa"}, {"abd"}} -> returnColsize = {2, 1};
+
 
 int	main(void)
 {
-	char *strs[] = {"eat", "tea", "tan", "ate", "nat", "bat"};
+	int	strcnt = 6;
+	char strs[7][4] = {"eat\0", "tea\0", "tan\0", "ate\0", "nat\0", "bat\0", 0};
+	int set_count;
+	int set_lens[6] = {0, 0, 0, 0, 0, 0};
+	char ***ret;
+
+	ret = groupAnagrams((char **)strs, strcnt, &set_count, (int**)set_lens);
+	free(ret);
+
 	return (0);
 	
 }
