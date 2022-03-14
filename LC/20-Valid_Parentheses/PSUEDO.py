cc0ced9d10439ed1b5bdf784a5e393f0aa7de986
junehan <junehan.dev@gmail.com>
2022-03-14 12:18:38 +0900

    PSUEDO

---
+++ LC/20-Valid_Parentheses/PSUEDO.py
@@ -0,0 +1,32 @@
+# 1
+Stack : callable(collection TYPE, SIZE, CONSTRAINTS:Optional);
+## stack.PUSH (x) NOT GOOD
+
+# 2
+Stack : callable(collection TYPE, SIZE);
+
+def is_paren(char):
+    if char in CONSTRAINTS:
+        return true
+    else 
+        return false
+
+def is_pushable(data:str):
+    flags = [0,0,0];
+    stack:Stack, 
+    stack = STACK(MAX_LEN);
+    for key in data:
+        char = ord(key);
+        if not is_paren(ch):
+            RET FALSE
+
+        if key == ('(' or ')') and not flags[0]:
+            PUSH KEY to STACK
+        elif key '[':
+            PUSH
+        elif key '{':
+            PUSH
+        else:
+            RET FALSE
+    retrun TRUE
+                    
