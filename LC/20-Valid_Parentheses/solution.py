4683559dff8a7c0ecae62666ec9b06707528edd7
junehan <junehan.dev@gmail.com>
2022-03-14 13:36:08 +0900

    20/HOTFIX:Fixed

--- LC/20-Valid_Parentheses/solution.py
+++ LC/20-Valid_Parentheses/solution.py
@@ -1,7 +1,7 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         try:
-            return check_valid(s);
+            return check_valid_2(s);
         except ValueError as e:
             print(e.args);
 #            raise e;
@@ -17,55 +17,60 @@ def is_paren(ch: str):
         return True
     raise ValueError();
 
-def check_valid(s: str):
+def check_valid_2(s: str):
     MAXLEN = 10000;
     stack = [];
     is_pushed = [False, False, False];
-    
+    print(s); 
     for ch in s:
         if not is_paren(ch):
             raise TypeError("not paren");
 
-        if ch in "()":
+        if ch in "({[":
             if ch == '(':
                 if is_pushed[0] is False:
                     is_pushed[0] = True;
                 else: 
                     print(s);
                     raise ValueError("( not closed");
-            else:
-                if is_pushed[0] is True:
-                    is_pushed[0] = False;
-                else: 
-                    print(s);
-                    raise ValueError("( not opened");
-        elif ch in "[]":
-            if ch == '[':
+            elif ch == '[':
                 if is_pushed[1] is False:
                     is_pushed[1] = True;
                 else: 
                     print(s);
                     raise ValueError("[ not closed");
             else:
-                if is_pushed[1] is True:
-                    is_pushed[1] = False;
-                else: 
-                    print(s);
-                    raise ValueError("[ not opened");
-        else:
-            if ch == '{':
-                if is_pushed[2] is False:
-                    is_pushed[2] = True;
+                if ch == '{':
+                    if is_pushed[2] is False:
+                        is_pushed[2] = True;
                 else: 
                     print(s);
                     raise ValueError("{ not closed");
-            else:
-                if is_pushed[2] is True:
-                    is_pushed[2] = False;
+            stack.append(ch);
+
+        else:
+            if ch == ')':
+                if is_pushed[0] is True and stack[-1] == '(':
+                    is_pushed[0] = False;
+                    stack.pop();
                 else: 
-                    print(s);
-                    raise ValueError("{ not opened");
-        stack.append(ch);
+                    print("close with", ch, ". stack:", stack);
+                    raise ValueError(")close order error");
+            elif ch == ']':
+                if is_pushed[1] is True and stack[-1] == '[':
+                    is_pushed[1] = False;
+                    stack.pop();
+                else: 
+                    print("close with", ch, ". stack:", stack);
+                    raise ValueError("]close order error");
+            else:
+                if is_pushed[2] is True and stack[-1] == '{':
+                    is_pushed[2] = False;
+                    stack.pop();
+                else: 
+                    print("close with", ch, ". stack:", stack);
+                    raise ValueError("}close order error");
+
         if len(stack) > MAXLEN:
             return IndexError("over limit");
 
@@ -78,5 +83,8 @@ if __name__ == "__main__":
     tests = "(),()[]{},(},(){".split(',');
     a_sol = Solution();
     results = list(map(a_sol.isValid, tests));
-    print(results);
-    
+    print(results, end="\n\n");
+    test = "({)}";
+    print(a_sol.isValid(test), end="\n\n");
+    test = "({})";
+    print(a_sol.isValid(test));
