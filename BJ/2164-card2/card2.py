1f4ef2cd13f3fe9e54e672da49c4b5a67432f0e8
junehan <junehan.dev@gmail.com>
2022-03-15 13:55:29 +0900

    2164/card2/Feat: new function using queue

--- BJ/2164-card2/card2.py
+++ BJ/2164-card2/card2.py
@@ -11,6 +11,16 @@ def card2(n):
             queue = [v for i, v in enumerate(queue) if i % 2];
     return (queue[0]);
 
+from collections import deque;
+def using_deque(n):
+    que = deque(range(1, n + 1));
 
+    while (len(que) > 1):
+        drop = que.popleft();
+        que.append(que.popleft());
+
+    return (que[0]);
+    
 if __name__ == "__main__":
     print(card2(get_input()));
+    print(using_deque(get_input()));
