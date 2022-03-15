82781003c60dbe0124f3268aa08342bd240af8ff
junehan <junehan.dev@gmail.com>
2022-03-15 12:25:54 +0900

    NEW/BJ/2164/card2

---
+++ BJ/2164-card2/card2.py
@@ -0,0 +1,17 @@
+def get_input():
+    return (int(input()));
+
+def card2(n):
+    queue = [_ for _ in n];
+    
+
+def enque(queue, data):
+    return (queue + [data]);
+    
+def deque(queue):
+    _, *queue = queue;
+    return (queue);
+
+if __name__ == "__main__":
+    for i in range(10);
+        print(i, ": ", card2(i));
