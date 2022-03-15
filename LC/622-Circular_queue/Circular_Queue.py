e9cc41d11322aa667b9df21fff026d1829e23bf5
junehan <junehan.dev@gmail.com>
2022-03-15 16:59:30 +0900

    622/CircularQ/Getter,Setter
    - FINISHED SPECS
    	- isEmpty() -> simple INDEX CMP
    	- Front() -> use getter and returns value inside object
    	- Rear() -> Same as above...
    
    - CUSTOMIZED
    	- front_obj.getter, rear_obj.getter returns LIST object
    	- above's setter take List from them and change tha value not List it self.
    		- setter used by Enque.
    		- getter used by Front and Rear, and they capture the value.

--- LC/622-Circular_queue/Circular_Queue.py
+++ LC/622-Circular_queue/Circular_Queue.py
@@ -1,38 +1,68 @@
 class MyCircularQueue:
-
     def __init__(self, k: int):
-        self.series = Tuple([[] for _ in range(k)]);
+        self._series = Tuple([[] for _ in range(k)]);
         self.capacity = k;
         self._front = 0;
         self._rear = 0;
 
     def enQueue(self, value: int) -> bool:
+        """
+        if self.isFull():
+            return False;
+        """
         pass
 
     def deQueue(self) -> bool:
-        pass
+        """
+        ret = self.front_obj.pop();
+        return front.pop()
+        """
 
-    def Front(self) -> int:
-        """Front : position of object inserted latest
-        Depends to the property _front
+    def isFull(self) -> bool:
+        """IF REAR NEXT IS FRONT? -> THEN FULL.
+        f = self.Front();
+        r = self.Rear();
+
+        if (f < r):
+            return True if (r-1 == f) else False;
+        if (f > r):
+            return True if (f-1 == r) else False;
+        return False;
         """
         pass
 
-    def Rear(self) -> int:
-        """Depends to the Property _rear
-        """
-        pass
-
+# PASS
     def isEmpty(self) -> bool:
         """IF FRONT EQUAL REAR -> THEN EMPTY.
         """
-        pass
+        return (self._front == self._rear);
 
-    def isFull(self) -> bool:
-        """IF REAR + 1 IS FRONT? -> THEN FULL.
-        """
-        pass
-        
+    def Front(self) -> int:
+        return (self.front_obj[0]);
+
+    def Rear(self) -> int:
+        return (self.rear_obj[0]);
+
+    @property
+    def front_obj(self) -> list:
+        ret = self_series[self._front];
+        assert(type(ret) == list);
+        return (ret);
+
+    @front_obj.setter(self, v):
+    def front_obj(self, v):
+        self.front_obj[0] = v;
+
+    @property
+    def rear_obj(self) -> list:
+        ret = self_series[self._rear];
+        assert(type(ret) == list);
+        return (ret);
+
+    @rear_obj.setter(self, v):
+    def rear_obj(self, v):
+        self.rear_obj[0] = v;
+
 # Your MyCircularQueue object will be instantiated and called as such:
 # obj = MyCircularQueue(k)
 # param_1 = obj.enQueue(value)
