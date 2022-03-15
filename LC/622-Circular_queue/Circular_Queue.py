03a164c67aa91fe58e38edbbc66bb32faa2de2e6
junehan <junehan.dev@gmail.com>
2022-03-15 19:11:31 +0900

    SpecChange/use size property
    - when have 3 space at first, enques new data, tail doesnt moves
    	- if size is 1 then tail and head will be indirect to same address.
        - size does measure when if tail and head is EQU, that is empty or not.

--- LC/622-Circular_queue/Circular_Queue.py
+++ LC/622-Circular_queue/Circular_Queue.py
@@ -1,81 +1,72 @@
-class MyCircularQueue:
+class MyCircularQueue_2:
     def __init__(self, k: int):
-        self._series = Tuple([[] for _ in range(k)]);
+        self._series = tuple([[] for _ in range(k)]);
         self.capacity = k;
         self._front = 0;
         self._rear = 0;
+        self.size = 0;
 
     def enQueue(self, value: int) -> bool:
         if self.isFull():
             return False;
-
-        r_idx = self._rear;
-        f_idx = self._front;
-        if (f_idx < r_idx):
-            expects = r_idx + 1;
-            queue_to = 0 if (expects == capacity) else expects;
-            self._rear = queue_to;
-            self.rear_obj = v;
-            return (True);
+        if self.size == 0:
+            self.front_obj.append(value);
         else:
-            self._rear += 1;
-            self._rear_obj = v;
-            return (True);
+            self._rear = self.get_next_r_idx();
+            self.rear_obj.append(value);
+        self.size += 1; 
+        print(self._series, self.size, "head:", self._front, "rear", self._rear);
+        return (True);
 
     def deQueue(self) -> bool:
-        """
-        ret = self.front_obj.pop();
-        return front.pop()
-        """
-
-    def isFull(self) -> bool:
-        """IF REAR NEXT IS FRONT? -> THEN FULL.
-        """
-        if self.isEmpty():
+        if self.size == 0:
             return False;
-
-        r_idx = self._rear;
-        f_idx = self._front;
-        if (f_idx < r_idx):
-            expects = r_idx + 1;
-            queue_to = 0 if (expects == capacity) else expects;
-            if queue_to == f_idx:
-                return (True)
-        else:
-            if (self._rear + 1 == self._front):
-                return (True);
-
-        return (False);
+        self.front_obj.pop();
+        self._front = self.get_next_f_idx();
+        self.size -=1;
+        print(self._series, self.size, "head:", self._front, "rear", self._rear);
+        return True;
 
 # PASS
+    def isFull(self) -> bool:
+        return (self.size == self.capacity);
+
+    def get_next_f_idx(self):
+        f = self._front;
+        return 0 if (f + 1 == self.capacity) else f + 1;
+
+    def get_next_r_idx(self):
+        r = self._rear;
+        return 0 if (r + 1 == self.capacity) else r + 1;
+
     def isEmpty(self) -> bool:
-        """IF FRONT EQUAL REAR -> THEN EMPTY.
-        """
-        return (self._front == self._rear);
+        return False if self.size else True;
 
     def Front(self) -> int:
-        return (self.front_obj[0]);
+        ret = self.front_obj;
+        return (ret[0] if ret else -1);
 
     def Rear(self) -> int:
-        return (self.rear_obj[0]);
+        ret = self.rear_obj;
+        return (ret[0] if ret else -1);
 
     @property
     def front_obj(self) -> list:
-        ret = self_series[self._front];
+        ret = self._series[self._front];
         assert(type(ret) == list);
         return (ret);
 
-    @front_obj.setter(self, v):
+    @front_obj.setter
     def front_obj(self, v):
         self.front_obj[0] = v;
 
     @property
     def rear_obj(self) -> list:
-        ret = self_series[self._rear];
+        ret = self._series[self._rear];
         assert(type(ret) == list);
         return (ret);
 
-    @rear_obj.setter(self, v):
+    @rear_obj.setter
     def rear_obj(self, v):
         self.rear_obj[0] = v;
 
@@ -88,8 +79,8 @@ class MyCircularQueue:
 # param_5 = obj.isEmpty()
 # param_6 = obj.isFull()
 
-def test()
-    MyCircularQueue myCircularQueue = MyCircularQueue(size);
+def test():
+    myCircularQueue = MyCircularQueue_2(3);
     assert(myCircularQueue.enQueue(1) == True)
     assert(myCircularQueue.enQueue(2) == True) #  True
     assert(myCircularQueue.enQueue(3) == True) #  True
@@ -97,11 +88,14 @@ def test()
     assert(myCircularQueue.Rear() == 3)        #  3
     assert(myCircularQueue.isFull() == True)   #  True
     assert(myCircularQueue.deQueue() == True)  #  True
+    assert(myCircularQueue.deQueue() == True)  #  True
     assert(myCircularQueue.enQueue(4) == True) #  True
+    assert(myCircularQueue.enQueue(4) == True) #  True
+    assert(myCircularQueue.deQueue() == True)  #  True
     assert(myCircularQueue.Rear() == 4)        #  4
 
 if __name__ == "__main__":
-    #test();
+    test();
         
 """
 Explanation
