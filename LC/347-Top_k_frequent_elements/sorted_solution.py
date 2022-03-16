1b0ece79e1b8c9d3e2a7e6d237ed74e33ed039a4
junehan <junehan.dev@gmail.com>
2022-03-16 16:43:52 +0900

    347/BUGFIX/Missed Operation after last loop
    - counter set is less than k, list appends not value change

--- LC/347-Top_k_frequent_elements/sorted_solution.py
+++ LC/347-Top_k_frequent_elements/sorted_solution.py
@@ -3,38 +3,44 @@ from typing import List
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        freq_map = [None] * k;
+        freq_map = [];
         counter = 0;
-        base = 1;
+        base = nums[0];
         set_count = 0;
-
-        i = 0;
         nums.append(None);
+        i = 0;
+        
         while (nums[i] != None):
             if nums[i] == base:
                 counter += 1;
             else:
                 if set_count < k:
-                    freq_map[set_count] = (base, counter); #UpDATE
+                    freq_map.append((base, counter)); #UpDATE
                 else:
-                    least_idx, least_value = find_least_freq(freq_map);
-                    if counter > least_value:
-                        freq_map[least_idx][0] = base;
-                        freq_map[least_idx][1] = counter;
-                    #else:
-                        #pass DO NOTHING
+                    least_idx, least_counter = find_least_freq(freq_map);
+                    if counter > least_counter:
+                        freq_map[least_idx] = (base, counter);
                 base = nums[i];
                 counter = 1;
                 set_count += 1;
             i += 1;
 
+        if not freq_map:
+            freq_map.append((base, counter));
+        else:
+            least_ids, least_counter = find_least_freq(freq_map);
+            if counter > least_counter:
+                freq_map[least_idx] = (base, counter);
+
         return (list(map(lambda el: el[0], freq_map)));
+
 
 def find_least_freq(freq_map):
     least_counter = freq_map[0][1];
     least_idx = 0;
-
     for i, freq_set in enumerate(freq_map):
+        if not freq_set:
+            return (i, 0)
         _, counter = freq_set;
         if counter < least_counter:
             least_idx = i
@@ -47,5 +53,9 @@ if __name__ == "__main__":
 
     data = [1,1,1,3,3,4,5,6];
     ret = a_sol.topKFrequent(data, 3);
+
+    data = [1];
+    ret = a_sol.topKFrequent(data, 1);
     
+
     print(ret);
