fec0920d935044c8c8bafd29d82053b2cf01a68b
junehan <junehan.dev@gmail.com>
2022-03-17 15:32:13 +0900

    TYPO

--- PG/42583/solution.py
+++ PG/42583/solution.py
@@ -30,7 +30,6 @@ if __name__ == "__main__":
     ret = solution(bl, w, trs);
     assert(ret == 8);
     bl, w, trs = [100, 100, [10]];
-
     ret = solution(bl, w, trs);
     assert(ret == 101);
     bl, w, trs = [100, 100, [10, 10, 10, 10, 10, 10, 10, 10, 10, 10]];
@@ -40,3 +39,32 @@ if __name__ == "__main__":
     ret = solution(bl, w, trs);
     assert(ret == 101);
 
+
+
+"""
+def solution(bridge_length, weight, truck_weights):
+    bridge_q = [0] * bridge_length;
+    answer = 0;
+    bridge_data = [0, bridge_q];
+    while(truck_weights or bridge_data[0] > 0):
+        t = truck_weights[0] if truck_weights else 0;
+        enqued = add_truck(bridge_data, weight, t);
+        if t and (t == enqued):
+           truck_weights = truck_weights[1:]
+        answer += 1;
+    
+    return answer
+
+def add_truck(b_data, max_w, to_add = 0):
+    cur_w = b_data[0];
+    out_w, *b_q = b_data[1];
+    cur_w -= out_w;
+    if not (cur_w + to_add < max_w + 1):
+        to_add = 0;
+    cur_w += to_add;
+    b_data[0] = cur_w;
+    b_q.append(to_add);
+    b_data[1] = b_q;
+    return to_add;
+
+"""
