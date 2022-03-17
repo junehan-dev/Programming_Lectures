1ccdc6b3e155eedd731c8fbfbae9c9d6491855ed
junehan <junehan.dev@gmail.com>
2022-03-18 05:19:13 +0900

    RESTART

---
+++ LC/21-merge_two_sorted_array/psuedo.py
@@ -0,0 +1,22 @@
+from collections import deque
+from typing import Optional
+from list_node import Node as ListNode
+
+class Solution:
+    def mergeTwoLists(self, s1: Optional[ListNode], s2: Optional[ListNode]) -> Optional[ListNode]:
+        
+        return (run(s1, s2));
+
+    
+    
+    while (s1 or s2): #[ s1, s1 ]
+        if s2.val > s1.val:
+            head_Q.enque(s1);
+            
+
+
+    
+
+if __name__ == "__main__":
+    s1 = ListNode([1,2,3]);
+    s2 = ListNode([1,3,5]);
--- LC/21-merge_two_sorted_array/solution.py
+++
@@ -1,4 +0,0 @@
-class Solution:
-    def mergeTwoLists(self, s1: Optional[ListNode], s2: Optional[ListNode]) -> Optional[ListNode]:
-        pass;
-
