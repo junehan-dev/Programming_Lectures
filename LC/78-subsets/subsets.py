64fa46692a4b14d6d3c39171dc5f6a950a620a2e
junehan <junehan.dev@gmail.com>
2022-03-19 14:46:29 +0900

    78/RUN

--- LC/78-subsets/subsets.py
+++ LC/78-subsets/subsets.py
@@ -1,13 +1,20 @@
-def subsets_dfs(dest, src):
-    if not src:
+def subsets_dfs(dest, src, k):
+    if not k:
         return [dest];
     ret = [];
+    if not src:
+        return [];
     for base in src:
+        """
         nsrc = [ch for ch in src if ch is not base];
-        combs = subsets_dfs(dest + [base], nsrc)
+        perms = subsets_dfs(dest + [base], nsrc)
+        ret += perms ;
+        """
+        combsrc = src[src.index(base) + 1:];
+        combs = subsets_dfs(dest + [base], combsrc, k-1)
         ret += combs;
     return (ret);
 
 if __name__ == "__main__":
-    ret =  subsets_dfs([], [1,2,3]);
+    ret =  subsets_dfs([], [1,2,3], 0);
     print(ret);
