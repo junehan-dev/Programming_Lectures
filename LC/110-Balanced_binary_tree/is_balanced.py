39ae50fd715af42d523d9957edae9c54440de7cf
junehan <junehan.dev@gmail.com>
2022-03-23 19:13:39 +0900

    FIX/light weight

--- LC/110-Balanced_binary_tree/is_balanced.py
+++ LC/110-Balanced_binary_tree/is_balanced.py
@@ -1,29 +1,21 @@
 from tree_node import TreeNode
 
-class HeightBalanceError(Exception):
-    """Raised When the height of tree differ more than one."""
-    pass
-
 def is_balanced(t):
-    if not t:
-        return True;
-    try:
-        dfs_height(t);
-    except HeightBalanceError as e:
+    log = [0];
+    dfs_height(t, log);
+
+    if log[0]:
         return False;
     return True;
 
-def dfs_height(root: TreeNode) -> bool:
+def dfs_height(root: TreeNode, log) -> bool:
+    if not root.left and not root.right:
+        return (0);
+
     l_h = (1 + dfs_height(root.left)) if root.left else 0;
     r_h = (1 + dfs_height(root.right)) if root.right else 0;
 
-    if (l_h + r_h):
-        l_h = l_h if l_h else r_h;
-        r_h = r_h if r_h else l_h;
+    if abs(l_h - r_h) > 1:
+        log[0] += 1;
+    return max((l_h, r_h));
 
-        if abs(l_h - r_h) > 1:
-            raise HeightBalanceError(f"{root}.left: {l_h}, right: {r_h}. non matched height found.");
-        return max((l_h, r_h));
-
-    return (0);
- 
