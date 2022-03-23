3dc14d6affe6dde1b644afc6749f5bd7d6c47be2
junehan <junehan.dev@gmail.com>
2022-03-23 18:49:17 +0900

    110/RUN

--- LC/110-Balanced_binary_tree/is_balanced.py
+++ LC/110-Balanced_binary_tree/is_balanced.py
@@ -5,23 +5,25 @@ class HeightBalanceError(Exception):
     pass
 
 def is_balanced(t):
+    if not t:
+        return True;
     try:
         dfs_height(t);
-    except ValueError as e:
+    except HeightBalanceError as e:
         return False;
     return True;
 
 def dfs_height(root: TreeNode) -> bool:
-    if not all([root.left, root.right]):
-        return (0);
-
     l_h = (1 + is_balanced(root.left)) if root.left else None;
     r_h = (1 + is_balanced(root.right)) if root.right else None;
-    l_h = l_h if l_h else r_h;
-    r_h = r_h if r_h else r_h;
-    
-    if abs(l_h - r_h) > 1:
-        raise HeightBalanceError(f"l_h: {l_h}, r_h: {r_h}");
 
+    if not any((l_h, r_h)):
+        return (0);
+
+    l_h = l_h if l_h is not None else r_h;
+    r_h = r_h if r_h is not None else l_h;
+
+    if abs(l_h - r_h) > 1:
+        raise HeightBalanceError(f"{root}.left: {l_h}, right: {r_h}. non matched height found.");
     return max((l_h, r_h));
-    
+ 
