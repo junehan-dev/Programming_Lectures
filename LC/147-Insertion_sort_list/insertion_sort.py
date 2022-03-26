494aee9c0f175b6d005bbb4f23fcb0ab9985a159
junehan <junehan.dev@gmail.com>
2022-03-26 14:57:27 +0900

    147/RUN

--- LC/147-Insertion_sort_list/insertion_sort.py
+++ LC/147-Insertion_sort_list/insertion_sort.py
@@ -4,22 +4,22 @@ def insertion_sort(head):
     if (head.next is None):
         return (head);
 
-    base_node = Node(None);
+    base_node = Node(None); # 1. None -> head
     base_node.next = head;
-    cur = base_node.next;
-    while (cur.next):
-        target_node = cur.next;
-        if (cur.val > target_node.val):
-            cur.next = target_node.next;
+    cur = base_node.next;   # 2. cur = head;
+    while (cur.next is not None):
+        if (cur.val <= cur.next.val):
+            cur = cur.next;
+        else: # cur.val > target.val
+            target = cur.next;
+            cur.next = target.next;
             prev_node = base_node;
             cmp_node = base_node.next;
-            while (cmp_node.val < target_node.val):
+            while (cmp_node.val < target.val):
                 prev_node = cmp_node;
                 cmp_node = cmp_node.next;
-            prev_node.next = target_node;
-            target_node.next = cmp_node;
-        else:
-            cur = target_node;
+            prev_node.next = target;
+            target.next = cmp_node;
     
     return (base_node.next);
 
