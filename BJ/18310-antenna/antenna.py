17d2370e19534ab745c970f00342d3b02cb520c6
junehan <junehan.dev@gmail.com>
2022-03-28 19:44:26 +0900

    FIN

--- BJ/18310-antenna/antenna.py
+++ BJ/18310-antenna/antenna.py
@@ -1,25 +1,56 @@
 def placement(cnt, houses):
-    qsort(houses, 0, cnt);
+    qsort(houses, 0, cnt - 1);
     return houses[(cnt - 1) // 2];
 
-def qsort(arr, low, high):
-    if len(arr[low:high]) > 1:
-        return None;
-    mid = ((low + high) // 2)
-    pivot = arr[mid];
-    start = low + 1;
-    end = high - 1;
 
-    arr[low], arr[mid] = arr[mid], arr[low];
-    while (start < end):
-        if start < end and arr[start] <= pivot:
-            start += 1;
-        if end > start and arr[end] > pivot:
-            end -= 1;
-        if arr[start] > pivot and arr[end] <= pivot:
-            arr[start], arr[end] = arr[end], arr[start];
 
-    start -= 1;
-    arr[low], arr[start] = arr[start], arr[low];
-    qsort(arr, low, start - 1);
-    qsort(arr, start + 1, high);
+
+
+
+def qsort(array, left, right):
+    if (right <= left): # 칸이 하나 남았다면 종료
+        return ;
+    print("======START=======");
+
+    lt_partition = left;
+# |(시작)<--적은 파티션의 경계--|PIVOTS|<--큰 파티션 경계(종료)|
+    cmp_index = lt_partition + 1; # 비교하는 인덱스의 주소
+    gt_partition = right;     # PIVOT보다 큰값의 파티션을 나누는 i
+    PIVOT = array[left]; # 매 호출마다 BOTTOM(0)번째
+    print("PIVOT:", PIVOT);
+
+    while (cmp_index <= gt_partition):
+        is_bigger = cmp(array[cmp_index], PIVOT);
+        if is_bigger < 0:
+                swap(array, lt_partition, cmp_index);
+                lt_partition += 1;
+                cmp_index += 1;
+        elif is_bigger > 0:
+                swap(array, cmp_index, gt_partition);
+                gt_partition -= 1;
+        else:
+            cmp_index += 1;
+
+
+    print("qsort left:[LEFT:lt+1]-",array[left:lt_partition]);
+    print("qsort fixed:[lt+1:gt+1]-", array[lt_partition:gt_partition +1]);
+    print("qsort right:[gt+1:RIGHT+1]-", array[gt_partition + 1:]);
+    print(array);
+    print("======FIN=======");
+    
+    qsort(array, left, lt_partition - 1);
+    qsort(array, gt_partition + 1, right);
+
+def swap(array, s1, s2):
+    array[s1], array[s2] = array[s2], array[s1];
+
+def cmp(c1, c2):
+    if c1.isnumeric():#숫자비교
+        return int(c1) - int(c2); 
+
+    else:#문자열 비교
+        if c1 > c2:
+            return 1;
+        elif c1 < c2:
+            return -1;
+        return 0;
--- BJ/18310-antenna/test.py
+++ BJ/18310-antenna/test.py
@@ -2,9 +2,15 @@ from antenna import placement
 from sys import stdin
 
 cnt = int(input());
+"""
 houses = [_ for _ in map(int, stdin.readline().split(' '))];
 print("cnt", cnt);
-print("houses",houses);
+print("houses", houses);
 ret = placement(cnt, houses);
+print("houses", houses);
 print(ret);
+"""
 
+chars = [v for v in stdin.readline().split(' ')];
+chars[-1] = chars[-1][0];
+ret = placement(cnt,chars);
