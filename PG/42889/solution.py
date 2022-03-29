e7619493ac30c1b1d38b3d61b4d5972f1fda829c
junehan <junehan.dev@gmail.com>
2022-03-29 18:02:03 +0900

    42889/RUN

--- PG/42889/solution.py
+++ PG/42889/solution.py
@@ -1,5 +1,34 @@
 from three_p_qsort import three_p_qsort
 
-a = [4,2,1,3,4,2,3,2,1,2,4,2,4,3,3,1];
-three_p_qsort(a, 0,  len(a)- 1);
-print(a);
+def calc_ratio(stages, n):
+    entered = 0;
+    cur = 0;
+    ret = {};
+    if stages[entered] == n + 1:
+        while (entered < len(stages) and stages[entered] == n+1):
+            entered+=1;
+    while n:
+        while entered + cur < len(stages) and stages[entered+cur] == n:
+            cur += 1;
+        entered += cur;
+        if f"{cur/entered}" in ret:
+            ret[f"{cur/entered}"].append(n);
+        else:
+            ret.update({f"{cur/entered}":[n]});
+        n -= 1;
+    return (ret);
+     
+def run(arr, n):
+    three_p_qsort(arr, 0, len(arr) - 1);
+    logs = calc_ratio(arr, n);
+    keys = sorted(logs.keys(), reverse=True);
+    ret = [];
+    for k in keys:
+        i = logs[k];
+        if len(i) > 1:
+            i.sort();
+        ret += i;
+    return (ret);
+
+print(run([1,1,1,1,1,1,1,1,1,1,1,1,1,1,2,3,2,2,1,3,2,4,4], 3));
+
