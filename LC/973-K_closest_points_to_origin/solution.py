1f22e0463e73d70d4337562fc9c7fffa119cf0bb
junehan <junehan.dev@gmail.com>
2022-03-30 16:56:55 +0900

    FIN

--- LC/973-K_closest_points_to_origin/solution.py
+++ LC/973-K_closest_points_to_origin/solution.py
@@ -9,6 +9,10 @@ class Solution:
         [[-2,2]];
         >>> Solution().kClosest([[3,3],[5,-1],[-2,4]], 2);
         [[3,3],[-2,4]]
+        >>> Solution().kClosest([[6,10],[-3,3],[-2,5],[0,2]], 3);
+        []
+        >>> Solution().kClosest([[-4, -7],[-4, -8],[-2,10],[10,7]], 3);
+        [[-4, -7],[-4,-8], [-2,10]]
         """
         min_q = build(points);
         return [del_min(min_q) for i in range(k)];
@@ -35,25 +39,29 @@ def insert(dest, src, cmp_func = (lambda a, b: a - b)):
     return (i - 1);
 
 def del_min(dest):
-    if not dest:
-        return None;
-    swap(dest, 0, -1);
-    ret = dest.pop();
-    i = 0;
-    size = len(dest);
-    while (not is_valid(dest, size, i)):
-        l = i * 2 + 1;
-        r = l + 1;
-        swap_i = l if is_far(dest[l], dest[r]) > 0 else r;
-        swap(dest, swap_i, i);
-        i = swap_i;
-    return (ret);
+    if dest:
+        swap(dest, 0, -1);
+        ret = dest.pop();
+        i = 0;
+        size = len(dest);
+        while (not is_valid(dest, size, i)):
+            l = i * 2 + 1;
+            r = l + 1;
+            if size < r + 1:
+                swap_i = l;
+            else:
+                swap_i = l if is_far(dest[l], dest[r]) <= 0 else r;
+            swap(dest, swap_i, i);
+            i = swap_i;
+        return (ret);
         
 def is_valid(src, size, i):
     l = i * 2 + 1;
     r = l + 1;
-    if (size < r + 1):
-       return True;
+    if (size < l + 1):
+        return True;
+    if size == l + 1:
+        return (is_far(src[i], src[l]) <= 0);
     return True if is_far(src[i], src[l]) <= 0 and is_far(src[i], src[r]) <= 0 else False;
 
 def swap(src, s1, s2):
