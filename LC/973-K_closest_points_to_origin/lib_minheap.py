fd86aeb6f379055ec84ac4ea77cfc7e67c9642bd
junehan <junehan.dev@gmail.com>
2022-03-30 17:07:21 +0900

    fix/lib_minheap.py

--- LC/973-K_closest_points_to_origin/lib_minheap.py
+++ LC/973-K_closest_points_to_origin/lib_minheap.py
@@ -1,6 +1,6 @@
-def build(srcs):
+def build(srcs, cmp_func = (lambda a, b: a - b)):
     ret = [];
-    [insert(ret, src) for src in srcs]
+    [insert(ret, src, cmp_func) for src in srcs]
 
     return (ret);
 
@@ -13,29 +13,33 @@ def insert(dest, src, cmp_func = (lambda a, b: a - b)):
 
     return (i - 1);
 
-def del_min(dest):
+def del_min(dest, cmp_func = lambda a, b: a - b):
     if not dest:
         return None;
     swap(dest, 0, -1);
     ret = dest.pop();
     i = 0;
     size = len(dest);
-    while (not is_valid(dest, size, i)):
+    while (not is_subtree_valid(dest, size, i, cmp_func)):
         l = i * 2 + 1;
         r = l + 1;
-        swap_i = l if dest[l] <= dest[r] else r;
+        if size < r + 1:
+            swap_i = l
+        else:
+            swap_i = l if dest[l] <= dest[r] else r;
         swap(dest, swap_i, i);
         i = swap_i;
     return (ret);
         
-def is_valid(src, size, i):
+def is_subtree_valid(src, size, i, cmp_func):
     l = i * 2 + 1;
     r = l + 1;
-    if (size < r + 1):
+    if (size < l + 1):
        return True;
-    return True if src[i] <= src[l] and src[i] <= src[r] else False;
+    if (size == l + 1):
+        return cmp_func(src[i], src[l]) <= 0;
+    return True if cmp_func(src[i] <= src[l]) and cmp_func(src[i] <= src[r]) else False;
 
 def swap(src, s1, s2):
     src[s1], src[s2] = src[s2], src[s1];
-
 
