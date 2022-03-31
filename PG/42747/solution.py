76aa1d5a3f9f605c3100821369eedbf4663f9797
junehan <junehan.dev@gmail.com>
2022-03-31 13:48:50 +0900

    PG-42747/FIN

--- PG/42747/solution.py
+++ PG/42747/solution.py
@@ -1,9 +1,19 @@
+from collections import Counter
 def solution(citations):
-    citations.sort(reverse=True);
-    for i in range(len(citations)):
-        if citations[i] <= i + 1:
-            return citations[i];
-    return (citations[i]);
-    
-print(solution([10,8]))
-#print(solution([1,0,6,5,5,5,0]));
+    c = Counter(citations);
+    keys = sorted(c.keys(), reverse=True)
+    sums = 0;
+    h_idx = 0;
+    for i, k in enumerate(keys):
+        sums += c[k];
+        c[k] = sums;
+        if sums >= k and h_idx <= k:
+            h_idx = k;
+        if sums < k and h_idx <= k:
+            h_idx = sums;
+        
+    return h_idx;
+
+#print(solution([10,8]))
+print(solution([1,0,6,5,5,5,0]))
+print(solution([1,0,6,5,5,5,0]) == 4);
