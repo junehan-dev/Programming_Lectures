534052c6e6d2a2bc3d9535dbae327ddd71ce9b81
junehan <junehan.dev@gmail.com>
2022-04-04 16:07:26 +0900

    BUGFIX

--- LC/743-Network_delay_time/test.py
+++ LC/743-Network_delay_time/test.py
@@ -4,8 +4,8 @@ from collections import deque
 def networkDelayTime(times: List[List[int]], n: int, k: int) -> int:
     signal_q, left = set_data(times, k);
     logs = [-1] * n;
-    print(logs);
     logs[k - 1] = 0;
+    print(logs);
     print(signal_q);
     print(left);
     delay_map = find_shortest_delay(signal_q, left, logs, 1);
@@ -31,18 +31,18 @@ def find_shortest_delay(signal_q, left, logs, n):
         print(signal_to);
         if logs[signal_to - 1] == -1 or logs[signal_to - 1] > delay:
             logs[signal_to - 1] = delay;
-            adjs = [_ for _ in filter(lambda sig: sig[0] == signal_to, left)];
+            adjs = [[] + _ for _ in filter(lambda sig: sig[0] == signal_to, left)];
             if adjs:
+                [sig for sig in adjs];
                 list(map(lambda sig: set_delay_acc(sig, delay), adjs));
                 [signal_q.append(signal) for signal in adjs];
-            else:
-                print("NOT!");
-            print(signal_q);
+            print("Q:",signal_q);
+            print("log:",logs);
 
     
 
     return logs;
-
+"""
 times = [[2,1,1],[2,3,1],[3,4,1]]; n = 4; k = 2;
 assert(networkDelayTime(times, n, k) == 2);
 
@@ -51,5 +51,11 @@ assert(networkDelayTime(times, n, k) == 1);
 
 times = [[1,2,1]]; n = 2; k = 2;
 assert(networkDelayTime(times, n, k) == -1);
+"""
+times = [[4,2,76],[1,3,79],[3,1,81],[4,3,30],[2,1,47],[1,5,61],[1,4,99],[3,4,68],[3,5,46],[4,1,6],[5,4,7],[5,3,44],[4,5,19],[2,3,13],[3,2,18],[1,2,0],[5,1,25],[2,5,58],[2,4,77],[5,2,74]];
+n = 5;
+k = 3;
 
+print(sorted(times, key=lambda el: el[0]));
+assert(networkDelayTime(times, n, k) == 59);
 
