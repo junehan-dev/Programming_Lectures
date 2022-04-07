6a4c3fd8bc9822accfbcffde68792346929e6b21
junehan <junehan.dev@gmail.com>
2022-04-07 10:54:28 +0900

    FIN

--- PG/43105/solution.py
+++ PG/43105/solution.py
@@ -1,17 +1,27 @@
+from functools import reduce
+
 def solution(triangle):
     prev_row, *triangle = triangle;
-    for row in triangle:
-        max_summation(prev_row, row);
-        prev_row = row;
-    answer = max(triangle[-1])
-    return answer
+    ret = reduce(max_summation, triangle, prev_row);
+    return (max(ret));
 
 def max_summation(row, next_row):
     new_row = [None] * len(next_row);
     for i, v in enumerate(row):
-        new_row[i] = (row[i] + next_row[i]) if new_row[i] is None else max([new_row[i], row[i] + next_row[i]]);
-        new_row[i+1] = (row[i] + next_row[i+1]) if new_row[i+1] is None else max([new_row[i], row[i] + next_row[i+1]]);
-    for i, v in enumerate(new_row):
-        next_row[i] = new_row[i];
+        new_row[i] = (
+            (row[i] + next_row[i]) if
+            new_row[i] is None else
+            max([new_row[i], row[i] + next_row[i]])
+        );
 
+        new_row[i+1] = (
+            (row[i] + next_row[i+1]) if
+            new_row[i+1] is None else
+            max([new_row[i], row[i] + next_row[i+1]])
+        );
 
+    return new_row;
+
+ret = solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]])
+
+print(ret);
