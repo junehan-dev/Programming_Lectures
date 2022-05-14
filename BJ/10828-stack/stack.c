6def77b9e933ee56154d0a29355d6a5b551a47ef
junehan <junehan.dev@gmail.com>
2022-05-14 16:57:38 +0900

    BJ/10828/STACK/PASS

--- BJ/10828-stack/stack.c
+++ BJ/10828-stack/stack.c
@@ -1,52 +1,71 @@
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
-
+#define MAX_BUF 13
+#define MAX_STACK_SIZ 5000
 enum CMDS	{ push = 1, pop, size, empty, top};
 
+int		read_line(FILE *r_stream, char *dest);
 int		itoa(char buf[], int n);
-int		get_num(void);
-int		get_cmd(void);
-void	read_int(int v);
+int		get_num(char *buf);
+int		get_cmd(char *buf);
+void	read_int(int v, char end);
 int		main()
 {
+	FILE *r_stream;
+	char buf[MAX_BUF];
+	int stack[MAX_STACK_SIZ];
 	int cur;
 	int max;
-	int	cmd;
+	char end;
 
-	max = get_num();
+	r_stream = fdopen(0, "r");
+	if (!r_stream)
+		return (1);
+	max = (read_line(r_stream, buf)) ? atoi(buf) : 0;
 	cur = 0;
-	int stack[10000];
-
-	while (max--) {
-		switch (get_cmd()) {
+	end = '\n';
+	while (max-- && read_line(r_stream, buf)) {
+		/*
+		 * end = max ? '\n': '\0';
+		 */
+		switch (get_cmd((char *)buf)) {
 			case push:
-				*(stack + cur++) = get_num();
+				*(stack + cur++) = get_num((char *)(buf + 5));
 				break;
 			case pop:
-				(cur > 0) ? read_int(*(stack + --cur)) : write(STDOUT_FILENO, "-1\n", 3);
+				(cur > 0) ? read_int(*(stack + --cur), end) : read_int(-1, end);
 				break;
 			case size:
-				cur ? read_int(cur): write(STDOUT_FILENO, "0\n", 2);
+				cur ? read_int(cur, end): read_int(0, end);
 				break;
 			case empty:
-				cur > 0 ? write(STDOUT_FILENO, "0\n", 2) : write(STDOUT_FILENO, "1\n", 2);
+				cur > 0 ? read_int(0, end) : read_int(1, end);
 				break;
 			default:
-				cur > 0 ? read_int(*(stack + cur - 1)) : write(STDOUT_FILENO, "-1\n", 3);
+				cur > 0 ? read_int(*(stack + cur - 1), end) :read_int(-1, end);
 		}
 	}
+	fclose(r_stream);
 	return (0);
 }
 
-
-void	read_int(int v)
+void	read_int(int v, char end)
 {
 	char temp[7];
 	int	size;
 
-	size = itoa(temp, v);
-	temp[size] = '\n';
-	write(STDOUT_FILENO, temp, size + 1);
+	if (v > 0) {
+		size = itoa(temp, v);
+		temp[size++] = end;
+	} else {
+		size = (v == 0) ? 2 : 3;
+		temp[0] = (v == 0) ? '0' : '-';
+		temp[1] = (v == 0) ? end : '1';
+		temp[2] = end;
+	} 
+	write(STDOUT_FILENO, temp, size);
 }
 
 int itoa(char *dest, int n)
@@ -66,33 +85,33 @@ int itoa(char *dest, int n)
 	return (i);
 }
 
-int		get_num(void)
+
+int		get_num(char *buf)
 {
 	char	key;
 	int		ret;
 
 	ret = 0;
-
-	while (read(STDIN_FILENO, &key, 1) && isdigit(key)) {
+	while (isdigit((key = *buf++))) {
 		ret *= 10;
 		ret += key - '0';
 	}
 	return ret;
 }
 
-int	get_cmd(void)
+int	get_cmd(char *cmd)
 {
-	char	buf[3];
-	char	key;
+	if (*(cmd + 0) == 'p')
+		return ((*(cmd + 1) == 'u') ? push : pop);
 
-	read(STDIN_FILENO, buf, 3);
-	while (read(STDIN_FILENO, &key, 1) && isalpha(key));
-
-	if (buf[0] == 'p')
-		return ((buf[1] == 'u') ? push : pop);
-
-	if (buf[0] == 's')
+	if (*(cmd + 0) == 's')
 		return (size);
 
-	return ((buf[0] == 'e') ? empty : top);
+	return ((*(cmd + 0) == 'e') ? empty : top);
 }
+
+int read_line(FILE *r_stream, char *dest)
+{
+	return (dest == fgets(dest, MAX_BUF, r_stream)) ? 1 : 0;
+}
+
