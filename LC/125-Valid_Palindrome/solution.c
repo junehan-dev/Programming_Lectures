6377d261e8f1ecf14f1bc179dd0b6681ceb1efd4
junehan <junehan.dev@gmail.com>
2022-07-22 17:34:26 +0900

    LC/125/Valid Palindrome/c/속도 개선

--- LC/125-Valid_Palindrome/solution.c
+++ LC/125-Valid_Palindrome/solution.c
@@ -12,13 +12,14 @@ bool isPalindrome(char *s)
 	ret = 1;
 	start = 0;
 	end = strlen(s) - 1;
-	while (ret && start <= end) {
-		while (start <= end && !((s_alnum = isalnum(*(s + start))) && (e_alnum = isalnum(*(s + end))))) {
+
+	while (ret && start < end) {
+		while (start < end && !((s_alnum = isalnum(*(s + start))) && (e_alnum = isalnum(*(s + end))))) {
 			start = !s_alnum ? start + 1 : start;
 			end = !e_alnum ? end - 1 : end;
 		}
 		
-		ret = (start <= end) ? (tolower(*(s + start++)) == tolower(*(s + end--))) : 1;
+		ret = (start < end) ? (tolower(*(s + start++)) == tolower(*(s + end--))) : 1;
 	}
 
 	return (ret);
