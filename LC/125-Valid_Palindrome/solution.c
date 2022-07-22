562e54d4326b812df93c3bf9aaa2e074201caf75
junehan <junehan.dev@gmail.com>
2022-07-22 17:33:22 +0900

    TYPO

--- LC/125-Valid_Palindrome/solution.c
+++ LC/125-Valid_Palindrome/solution.c
@@ -5,19 +5,19 @@ bool isPalindrome(char *s)
 {
 	ssize_t end;
 	ssize_t start;
-    char s_alnum;
-    char e_alnum;
-    bool ret;
+	char s_alnum;
+	char e_alnum;
+	bool ret;
 
 	ret = 1;
 	start = 0;
 	end = strlen(s) - 1;
 	while (ret && start <= end) {
 		while (start <= end && !((s_alnum = isalnum(*(s + start))) && (e_alnum = isalnum(*(s + end))))) {
-		    start = !s_alnum ? start + 1 : start;
-            end = !e_alnum ? end - 1 : end;
+			start = !s_alnum ? start + 1 : start;
+			end = !e_alnum ? end - 1 : end;
 		}
-        
+		
 		ret = (start <= end) ? (tolower(*(s + start++)) == tolower(*(s + end--))) : 1;
 	}
 
